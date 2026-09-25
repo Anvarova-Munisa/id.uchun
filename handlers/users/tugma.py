44113fce844b477fb9b624c4b007fdbd6a22db14
Munisa <Munisa@example.com>
2026-09-25 16:05:06 +0300

    yangilandi

--- handlers/users/tugma.py
+++ handlers/users/tugma.py
@@ -8,27 +8,9 @@ async def button(msg:types.Message):
 
 @router.callback_query(F.data == "oddiy")
 async def oddiy(callback:types.CallbackQuery):
-    await callback.answer("mazza qildingizmi",show_alert=True)
+    await callback.answer("mazza qildingizmi")
     # await callback.message.edit_text("yana tanlashga aqliz yetadimi",reply_markup=tugma())
     # await callback.message.delete()
-
-# @router.callback_query(F.data == "car")
-# async def car2(callback:types.CallbackQuery):
-#     await callback.answer("mashinalardan birini tanlang",reply_markup=car())
-# @router.callback_query(F.data == "tanlandi_")
-# async def tanlandi(callback:types.CallbackQuery):
-#     car_name = callback.data.replace("tanlandi_","")
-#     await callback.answer(" nasib qilsin")
-
-
-# @router.callback_query(F.data == "car")
-# async def car2(callback: types.CallbackQuery):
-#     await callback.answer()
-#     await callback.message.edit_text("Mashinalardan birini tanlang", reply_markup=car())
-# @router.callback_query(F.data.startswith("tanlandi_"))
-# async def tanlandi1(callback: types.CallbackQuery):
-#     car_name = callback.data.replace("tanlandi_", "")
-#     await callback.answer(f"{car_name.capitalize()} nasib qilsin!", show_alert=True)
 
 @router.callback_query(F.data == "car")
 async def car2(callback: types.CallbackQuery):
